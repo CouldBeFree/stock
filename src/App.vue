4f64f9db45ce8fef69b0e0c8f055b363a0732d33
CouldBeFree <propokerstar90@gmail.com>
2019-04-13 19:15:19 +0300

    Created header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
     <div class="container">
-        <router-view></router-view>
+        <app-header/>
+        <div class="row">
+            <div class="col-xs-12">
+                <router-view></router-view>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-
+    import Header from './components/Header.vue';
     export default {
-
+        components: {
+            appHeader: Header
+        }
     }
 </script>
 
-<style>
-
+<style scoped>
+    body{
+        padding: 30px;
+    }
 </style>
