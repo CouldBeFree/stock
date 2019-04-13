4b5e5d167264e8ff1b50317ccca83f18fb7ca5c8
CouldBeFree <propokerstar90@gmail.com>
2019-04-13 19:30:23 +0300

    Created stock

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,26 @@
 <template>
-    <h1>I am the stocks-stocks component</h1>
+    <div>
+        <stock v-for="stock in stocks"></stock>
+    </div>
 </template>
 
 <script>
+    import Stock from './Stock.vue'
+
     export default {
-        name: "Stocks"
+        data(){
+            return{
+                stocks: [
+                    { id:1, name: 'BMW', price:110 },
+                    { id:2, name: 'Google', price:200 },
+                    { id:3, name: 'Apple', price:250 },
+                    { id:4, name: 'Twitter', price:8 }
+                ]
+            }
+        },
+        components:{
+            'stock': Stock
+        }
     }
 </script>
 
